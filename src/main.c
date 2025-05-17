a89291ce6fb8b0f3f7a6b34b95fa0910cb32c37f
DaliBerr <1593316231@qq.com>
2025-05-18 01:32:50 +0200

    feature scale bilinear

--- src/main.c
+++ src/main.c
@@ -53,6 +53,10 @@ int main(int argc, char **argv) {
       scale_nearest(configuration.filenames[0],
         atof(configuration.arguments[0]));
   } 
+  if ( strncmp( configuration.command, "scale_bilinear", 14 ) == 0 ) {
+      scale_nearest(configuration.filenames[0],
+        atof(configuration.arguments[0]));
+  }   
   /*
    * TO COMPLETE
    */
