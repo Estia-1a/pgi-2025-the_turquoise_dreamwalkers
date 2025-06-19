0181302f49b9d3ea0dd6593e77facd6fa2e98f31
Jv0dgs <2981559805@qq.com>
2025-06-19 10:59:46 +0200

    feat:Min-pixel /fix :utils bug

--- src/features.h
+++ src/features.h
@@ -19,4 +19,5 @@ void second_line(char *source_path);
 //void yifei
 
 void tenth_pixel (char *source_path);
+void min_pixel (char *source_path);
 #endif
--- src/main.c
+++ src/main.c
@@ -86,6 +86,9 @@ int main(int argc, char **argv) {
       tenth_pixel(configuration.filenames[0]);
   }
   
+  if(strncmp(configuration.command,"min_pixel",9)==0){
+      min_pixel(configuration.filenames[0]);
+  }
   /*
    * TO COMPLETE
    */
