ff3abc9a2e1eee1bcdf77ce5237d2fdb254ae763
AdamMoukla-tech <adam.moukla@etu.estia.fr>
2025-06-19 17:15:31 +0200

    123

--- src/features.h
+++ src/features.h
@@ -24,4 +24,6 @@ void tenth_pixel (char *source_path);
 void color_blue(char *source_path);
 void color_red(char* source_path);
 void min_pixel (char *source_path);
+void color_green(char *source_path);
+
 #endif
