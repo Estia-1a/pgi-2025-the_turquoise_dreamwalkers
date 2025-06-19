a2ab3a34e346b4355b224b0c38f794d8263c705e
AdamMoukla-tech <adam.moukla@etu.estia.fr>
2025-06-19 17:20:48 +0200

    color_gray

--- src/features.h
+++ src/features.h
@@ -25,5 +25,7 @@ void color_blue(char *source_path);
 void color_red(char* source_path);
 void min_pixel (char *source_path);
 void color_green(char *source_path);
+void color_gray(char *source_path);
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -94,6 +94,10 @@ int main(int argc, char **argv) {
   if (strncmp(configuration.command, "color_red",9) == 0) {
     color_red(configuration.filenames[0]);
  }
+ if (strncmp(configuration.command, "color_gray", 10) == 0) {
+    color_gray(configuration.filenames[0]);
+}
+
   /*
    * TO COMPLETE
    */
